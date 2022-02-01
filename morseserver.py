5b744799d19118060adcbbc4403516f1bb692333
Eva-Maria Hofer <eva-maria.hofer@bulme.at>
2022-02-01 22:18:38 +0100

    output : morsecode

--- morseserver.py
+++ morseserver.py
@@ -2,6 +2,25 @@
 
 import socketserver
 import sys
+import codecs
+
+morse ={"1" : ".----",
+        "2" : "..---",
+        "3" : "...--",
+        "4" : "....-",
+        "5" : ".....",
+        "6" : "-....",
+        "7" : "--...",
+        "8" : "---..",
+        "9" : "----.",
+        "0" : "-----",
+        "a" : ".-",
+        "b" : "-...",
+        "c" : "-.-.",
+        "d" : "-..",
+        "e" : ".",
+        "f" : "..-."
+}
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
@@ -16,14 +35,22 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        MyMorseHandler(self.data)
         # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        #self.request.sendall(self.data.upper())
+        
+def MyMorseHandler(data):
+        try:
+            d = codecs.decode(data)   #aus binary code einen string machen
+            for zeichen in d:
+                print(morse[zeichen])
+        except KeyError as err:
+            print("Nicht im Code enthalten:", err.args[0])
 
 if len(sys.argv) !=2:
     raise ValueError("Bitte Port angeben")
 
-port1 = int(sys.argv[1])
+port1 = int(sys.argv[1])    #erstes argument ist name von script, zweites ist Wert
 
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", port1
